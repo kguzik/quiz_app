5d061caf6aeeb107aba03ac45189ea483dc9aa6d
Karolina Guzik <karolina.guzik08@gmail.com>
2019-04-06 15:07:30 +0200

    basic quiz structure

---
+++ script/global.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    changeQuestion();
+});
+
+function changeQuestion() {
+    let questions = document.getElementsByClassName('question-card');
+    let submit = document.getElementById('submit-input');
+    let buttonNext = document.getElementsByClassName('btn-next');
+    submit.style.display = 'none';
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].className += ' question' + (1 + i);
+        if (i === 0) {
+            questions[0].style.display = 'block';
+        } else {
+            questions[i].style.display = 'none';
+        }
+    }
+
+    for (let i = 0; i < buttonNext.length; i++) {
+        buttonNext[i].addEventListener('click', function () {
+            if (i + 1 === buttonNext.length - 1) {
+                submit.style.display = 'block';
+                this.parentNode.style.display = 'none';
+                buttonNext[i + 1].parentNode.style.display = 'block';
+                buttonNext[i + 1].style.display = 'none';
+                return;
+            }
+            this.parentNode.style.display = 'none';
+            this.removeAttribute('id');
+            buttonNext[i + 1].parentNode.style.display = 'block';
+            buttonNext[i + 1].setAttribute('id', 'current-question');
+        });
+    }
+}
